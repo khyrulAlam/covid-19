a5d8340760763f58bf6c826fb7eb4e2243804169
khyrulAlam <khyrulalam69@gmail.com>
2020-05-03 13:10:21 +0600

    api endpoint version change

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const API = {
-  COVID_API: 'https://corona.lmao.ninja',
-  CONVID_API_ALL: 'https://corona.lmao.ninja/all',
-  CONVID_API_COUNTRIES: 'https://corona.lmao.ninja/countries/',
+  COVID_API: 'https://corona.lmao.ninja/v2',
+  CONVID_API_ALL: 'https://corona.lmao.ninja/v2/all',
+  CONVID_API_COUNTRIES: 'https://corona.lmao.ninja/v2/countries/',
   CONVID_API_COUNTRIES_HISTORICAL: 'https://corona.lmao.ninja/v2/historical/',
   BOOKMARK_KEY: 'bookmark_countries'
 };
